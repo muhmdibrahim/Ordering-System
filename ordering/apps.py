0e4d1d5c90fbf15915f95116f0cf26d72e96cdc2
muhmdibrahim <mibrahimmanagement@gmail.com>
2025-07-15 07:04:01 +0300

    signals file to create user when log in if not exist

--- ordering/apps.py
+++ ordering/apps.py
@@ -3,6 +3,9 @@ from django.apps import AppConfig
 class OrderingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ordering'
+
+def ready(self):
+    import ordering.signals
 '''
 class SuitConfig(DjangoSuitConfig):
     layout = 'horizontal
